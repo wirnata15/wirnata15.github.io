e4139e93bc52e7bbc5537e9fad6b02d790042145
Agung <agungchandranata@gmail.com>
2021-10-11 22:51:45 +0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, SyntheticEvent, useEffect, useRef, useState } from "react";
+import Bonk1 from "./assets/bonk1.png";
+import Bonk2 from "./assets/bonk2.png";
+import "./App.css";
+import { Howl, Howler } from "howler";
+import { Grid, Image } from "semantic-ui-react";
 
-function App() {
+const audio = new Audio("./bonk.mp3");
+
+const App: FC = () => {
+  const [count, setCount] = useState(0);
+  const [bonking, setBonking] = useState(false);
+  const addCount = (e: SyntheticEvent) => {
+    e.preventDefault();
+    audio.play();
+    setBonking(true);
+    setCount(count + 1);
+  };
+  const releaseBonk = (e: SyntheticEvent) => {
+    e.preventDefault();
+    // audio.pause();
+    // audio.currentTime = 0;
+    setBonking(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="counter">Times Bonked: {count}</h1>
+      <Grid centered>
+        <Grid.Row>
+          <Grid.Column largeScreen={8} widescreen={8} mobile={10}>
+            <Image
+              src={bonking ? Bonk2 : Bonk1}
+              onTouchStart={(e: SyntheticEvent) => addCount(e)}
+              onTouchEnd={(e: SyntheticEvent) => releaseBonk(e)}
+              onMouseDown={(e: SyntheticEvent) => addCount(e)}
+              onMouseUp={(e: SyntheticEvent) => releaseBonk(e)}
+              fluid
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      {/* <div className="imageContainer"></div> */}
     </div>
   );
-}
+};
 
 export default App;
